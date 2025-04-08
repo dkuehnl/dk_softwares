6e1fa6ab7de652e2ee361ff62a410ae07a41ef1e
Dennis <d.kuehnlein@outlook.com>
2025-04-08 09:58:10 +0200

    brand new working setup with vue, vue-router, tailwindcss and flowbite-vue

---
+++ dk-softwares/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterView, RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <div class="p-4">
+    <nav class="flex gap-4 mb-4">
+      <RouterLink to="/" class="text-blue-600">Home</RouterLink>
+      <RouterLink to="/about" class="text-blue-600">About</RouterLink>
+    </nav>
+    <RouterView />
+  </div>
+</template>
---
+++ dk-softwares/src/views/Home.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { FwbButton } from 'flowbite-vue'
+</script>
+
+<template>
+  <div>
+    <h1 class="text-2xl font-bold mb-4">Home</h1>
+    <FwbButton color="green">Click me</FwbButton>
+  </div>
+</template>
