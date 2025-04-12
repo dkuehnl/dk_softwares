4f4a5eec6452936c2b86d9eb8fa671ae4f71e51e
Dennis Kühnlein <d.kuehnlein@outlook.com>
2025-04-12 21:00:18 +0200

    build navbar and start home-page

--- dk-softwares/src/App.vue
+++ dk-softwares/src/App.vue
@@ -1,13 +1,37 @@
 <script setup>
-import { RouterView, RouterLink } from 'vue-router'
+  import { RouterView, RouterLink } from 'vue-router'
+  import {
+    FwbNavbar,
+    FwbNavbarCollapse,
+    FwbNavbarLink,
+    FwbNavbarLogo,
+  } from 'flowbite-vue'
 </script>
 
 <template>
-  <div class="p-4">
-    <nav class="flex gap-4 mb-4">
-      <RouterLink to="/" class="text-blue-600">Home</RouterLink>
-      <RouterLink to="/about" class="text-blue-600">About</RouterLink>
-    </nav>
-    <RouterView />
-  </div>
+  <fwb-navbar solid>
+    <template #logo>
+      <fwb-navbar-logo alt="Flowbite logo" image-url="vite.svg" link="#">
+        dk software solutions
+      </fwb-navbar-logo>
+    </template>
+    <template #default="{isShowMenu}">
+      <fwb-navbar-collapse :is-show-menu="isShowMenu">
+        <fwb-navbar-link is-active link="#">
+          <RouterLink to="/" class="text-blue-600">Home</RouterLink>
+        </fwb-navbar-link>
+        <fwb-navbar-link link="#">
+          <RouterLink to="/about" class="text-blue-600">About</RouterLink>
+        </fwb-navbar-link>
+        <fwb-navbar-link link="#">
+          Pricing
+        </fwb-navbar-link>
+        <fwb-navbar-link link="#">
+          Contact
+        </fwb-navbar-link>
+      </fwb-navbar-collapse>
+    </template>
+  </fwb-navbar>
+
+  <RouterView/>
 </template>
--- dk-softwares/src/views/Home.vue
+++ dk-softwares/src/views/Home.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { FwbButton } from 'flowbite-vue'
+  import { FwbButton} from 'flowbite-vue'
+
 </script>
 
 <template>
